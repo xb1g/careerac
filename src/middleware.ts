44f2de215fbd22f336435dc7af22043ac7636c66
Bunyasit Fang <wachaa1319@gmail.com>
2026-04-16 01:39:46 -0700

    fix: move redirect from protected/page.tsx to middleware to fix build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,12 @@
 import { updateSession } from "@/utils/supabase/middleware";
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  
   return await updateSession(request);
 }
 
